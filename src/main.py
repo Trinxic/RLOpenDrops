203796234f0a18a746a6e1ebf726583a66e744cc
Trinxic <davida_2016@outlook.com>
2025-03-26 06:38:46 -0400

    Moved things around. Getting ready to turn this into a full app.

--- src/main.py
+++ src/main.py
@@ -3,22 +3,23 @@ from os import makedirs, path
 from random import uniform
 from threading import Thread
 from time import sleep
+from typing import Optional
 
-import pytesseract
 from keyboard import wait
-from PIL import ImageGrab
+from PIL import Image, ImageGrab
 from pyautogui import getWindowsWithTitle, press
+from pytesseract import image_to_string
 
 RUNNING: bool = False
 EXITING: bool = False
 TOGGLE_KEY: str = "t"
 EXIT_KEY: str = "e"
-DROP_COUNT: int = None
-CURSOR_UP = "\033[1A"
-CLEAR_LINE = "\x1b[2K"
+DROP_COUNT: Optional[int] = None
+CURSOR_UP: str = "\033[1A"
+CLEAR_LINE: str = "\x1b[2K"
 
 
-def get_rocket_league_window():  # -> <whatever object `windows[0]` is>
+def get_rocket_league_window() -> Optional[object]:
     windows = getWindowsWithTitle("Rocket League")
     if not windows:
         print("Rocket League window not found!")
@@ -26,42 +27,42 @@ def get_rocket_league_window():  # -> <whatever object `windows[0]` is>
     return windows[0]
 
 
-def save_screenshot(screenshot) -> None:
-    script_dir = path.dirname(path.abspath(__file__))
-    save_dir = path.join(script_dir, "drops-screenshots")
+def save_screenshot(screenshot: Image.Image) -> None:
+    script_dir: str = path.dirname(path.abspath(__file__))
+    save_dir: str = path.join(script_dir, "drops-screenshots")
 
     makedirs(save_dir, exist_ok=True)  # only creates if it doesn't exist already
 
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    filename = f"drops_screenshot_{timestamp}.jpeg"
-    filepath = path.join(save_dir, filename)
+    timestamp: str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    filename: str = f"drops_screenshot_{timestamp}.jpeg"
+    filepath: str = path.join(save_dir, filename)
 
     screenshot.save(filepath, "JPEG")
     print(f"Screenshot saved: {filepath}")
 
 
-def get_drops_count() -> int:
+def get_drops_count() -> Optional[int]:
     global DROP_COUNT
     window = get_rocket_league_window()
     if not window:
         return None
 
     # get dimensions of screenshot
-    left_bound = window.left + int(window.width * 0.15625)  # 5/32
-    top_bound = window.top + int(window.height * 0.1851852)  # 5/27
-    right_bound = window.left + int(window.width * 0.21875)  # 7/32
-    bottom_bound = window.top + int(window.height * 0.225)  # 9/40
+    left_bound: int = window.left + int(window.width * 0.15625)  # 5/32
+    top_bound: int = window.top + int(window.height * 0.1851852)  # 5/27
+    right_bound: int = window.left + int(window.width * 0.21875)  # 7/32
+    bottom_bound: int = window.top + int(window.height * 0.225)  # 9/40
 
     # ic(window.left, window.top, window.right, window.bottom)
     # ic(window.width, window.height, left_bound, top_bound, right_bound, bottom_bound)
 
-    screenshot = ImageGrab.grab(bbox=(left_bound, top_bound, right_bound, bottom_bound))
+    screenshot: Image.Image = ImageGrab.grab(
+        bbox=(left_bound, top_bound, right_bound, bottom_bound)
+    )
     save_screenshot(screenshot)
 
-    num_drops = (
-        pytesseract.image_to_string(screenshot, config="--psm 6")
-        .strip()
-        .replace("x", "")
+    num_drops: str = (
+        image_to_string(screenshot, config="--psm 6").strip().replace("x", "")
     )
 
     try:
@@ -74,7 +75,7 @@ def get_drops_count() -> int:
     return DROP_COUNT
 
 
-def open_drops():
+def open_drops() -> None:
     global RUNNING, EXITING, DROP_COUNT
 
     if RUNNING and not EXITING:
@@ -90,7 +91,7 @@ def open_drops():
             break
 
         print(CURSOR_UP + CLEAR_LINE, end="")
-        print(f"{DROP_COUNT} drop{'' if DROP_COUNT == 0 else 's'} left")
+        print(f"{DROP_COUNT} drop{'' if DROP_COUNT == 1 else 's'} left")
 
         press("enter")  # click "OPEN DROP"
         sleep(uniform(0.2, 0.4))
@@ -105,9 +106,9 @@ def open_drops():
             DROP_COUNT -= 1
 
 
-def listen_for_key():
+def listen_for_key() -> None:
     global RUNNING
-    worker = None  # keep track of running thread
+    worker: Optional[Thread] = None  # keep track of running thread
 
     while True:
         wait(TOGGLE_KEY)
@@ -119,7 +120,7 @@ def listen_for_key():
             worker.start()
 
 
-def main():
+def main() -> None:
     print('ATTENTION: Ensure you are at the "Possible Contents" screen!')
 
     Thread(target=listen_for_key, daemon=True).start()
---
+++ todo.md
@@ -0,0 +1,5 @@
+ [] Figure out venv or whatever I decide to use
+ [] populate /src python files
+ [] Create GUI
+ [] create executable file
+ [] create [setup file](https://youtu.be/uji8ZRBd2tw?si=HvuyEv8BcPXcdXEC)
